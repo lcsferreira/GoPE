6366bdd11276a255116ba081b16308e4406d1685
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2024-02-15 15:02:42 -0300

    feat: add modal for confirmation of action

---
+++ src/components/modalCard.php
@@ -0,0 +1,16 @@
+<div class="modal">
+  <div class="modal-content">
+    <div class="modal-header">
+      <div class="icon">
+        <i class="<?php echo $icon; ?>"></i>
+      </div>
+    </div>
+    <div class="modal-body">
+      <h2><?php echo $message; ?></h2>
+    </div>
+    <div class="modal-footer">
+      <button class="btn" id="modal-confirm"><?php echo $buttonConfirmText; ?></button>
+      <button class="btn-close" id="modal-close"><?php echo $buttonCloseText; ?></button>
+    </div>
+  </div>
+</div>
