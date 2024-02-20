9b32db6fa01846e460b93deb2b309903e2ded91d
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2024-02-20 09:59:25 -0300

    fix: fix header contact redirects

--- src/components/header.php
+++ src/components/header.php
@@ -19,27 +19,29 @@
   </div>
   <ul class="sidebar__links">
     <?php if ($_SESSION['type'] === 'admin') : ?>
-      <li><a href="../../../src/pages/Dashboard/usersList.php"> Users List</a></li>
-      <li><a href="../../../src/pages/Dashboard/countriesList.php">Countries List</a></li>
-      <li><a href="../../../src/pages/Guide/manualGuide.php">Guide</a></li>
+    <li><a href="../../../src/pages/Dashboard/usersList.php"> Users List</a></li>
+    <li><a href="../../../src/pages/Dashboard/countriesList.php">Countries List</a></li>
+    <li><a href="../../../src/pages/Guide/manualGuide.php">Guide</a></li>
     <?php else : ?>
-      <li><a href="countries.php?id=<?php echo $_SESSION['id']; ?>">Countries list</a></li>
+    <li><a href="../../../src/pages/Dashboard/countriesList.php?id=<?php echo $_SESSION['id']; ?>">Countries list</a>
+    </li>
+    <li><a href="../../../src/pages/Guide/manualGuide.php">Guide</a></li>
     <?php endif; ?>
   </ul>
 </div>
 
 <!-- JavaScript to handle sidebar toggle -->
 <script>
-  const openSidebarButton = document.getElementById('openSidebar');
-  const sidebar = document.getElementById('sidebar');
+const openSidebarButton = document.getElementById('openSidebar');
+const sidebar = document.getElementById('sidebar');
 
-  openSidebarButton.addEventListener('click', () => {
-    sidebar.classList.add('open');
-  });
+openSidebarButton.addEventListener('click', () => {
+  sidebar.classList.add('open');
+});
 
-  const closeSidebarButton = document.getElementById('closeSidebar');
+const closeSidebarButton = document.getElementById('closeSidebar');
 
-  closeSidebarButton.addEventListener('click', () => {
-    sidebar.classList.remove('open');
-  });
+closeSidebarButton.addEventListener('click', () => {
+  sidebar.classList.remove('open');
+});
 </script>
