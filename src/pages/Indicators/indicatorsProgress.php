d2b6e60bb8369cf5ace0a422a97a336cccd5e494
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2024-02-21 11:21:25 -0300

    fix: fix redirect of next button

--- src/pages/Indicators/indicatorsProgress.php
+++ src/pages/Indicators/indicatorsProgress.php
@@ -20,8 +20,10 @@ if (!isset($_SESSION['loggedIn'])) {
   <link rel="stylesheet" href="../../css/components/header.css">
   <link rel="stylesheet" href="../../css/components/modal.css">
   <link rel="stylesheet" href="../../css/pages/indicators.css">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/solid.css" integrity="sha384-Tv5i09RULyHKMwX0E8wJUqSOaXlyu3SQxORObAI08iUwIalMmN5L6AvlPX2LMoSE" crossorigin="anonymous" />
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/fontawesome.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/solid.css"
+    integrity="sha384-Tv5i09RULyHKMwX0E8wJUqSOaXlyu3SQxORObAI08iUwIalMmN5L6AvlPX2LMoSE" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/fontawesome.css"
+    integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous" />
 </head>
 
 <body>
@@ -90,21 +92,21 @@ if (!isset($_SESSION['loggedIn'])) {
   </div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script>
-    const buttonNext = document.querySelector('.btn-next');
-    const buttonBack = document.querySelector('.btn-back');
+  const buttonNext = document.querySelector('.btn-next');
+  const buttonBack = document.querySelector('.btn-back');
 
-    buttonNext.addEventListener('click', () => {
-      window.location.href = 'demographicData.php';
-    });
+  buttonNext.addEventListener('click', () => {
+    window.location.href = 'demographicData.php?id=<?php echo $_GET['id'] ?>';
+  });
 
-    buttonBack.addEventListener('click', () => {
-      window.location.href = '../Dashboard/countriesList.php<?php if ($_SESSION['type'] == 'admin') {
+  buttonBack.addEventListener('click', () => {
+    window.location.href = '../Dashboard/countriesList.php<?php if ($_SESSION['type'] == 'admin') {
                                                               echo "'";
                                                             } else {
                                                               echo "?id=" . $_SESSION['id'] . "'";
                                                             }
                                                             ?>
-    });
+  });
   </script>
 </body>
 
