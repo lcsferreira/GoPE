c07272ec2219dc6e1c47a6aa96afd3240d863af1
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2024-02-25 19:41:14 -0300

    fix: add correct url links at side nav

--- src/components/sideNavBar.php
+++ src/components/sideNavBar.php
@@ -2,16 +2,16 @@
   <ul class="side-nav__links">
     <li <?php if (basename($_SERVER['PHP_SELF']) == "demographicData.php") echo 'class="selected"' ?>><a
         href="../../../src/pages/Indicators/demographicData.php?id=<?php echo $_GET["id"]; ?>">Demographic data</a></li>
-    <li <?php if (basename($_SERVER['PHP_SELF']) == "physicalActivity.php") echo 'class="selected"' ?>><a
+    <li <?php if (basename($_SERVER['PHP_SELF']) == "paPrevalence.php") echo 'class="selected"' ?>><a
         href="../../../src/pages/Indicators/paPrevalence.php?id=<?php echo $_GET["id"]; ?>">Physical activity
         prevalence</a></li>
-    <li <?php if (basename($_SERVER['PHP_SELF']) == "physicalEducationPolicy.php") echo 'class="selected"' ?>><a
+    <li <?php if (basename($_SERVER['PHP_SELF']) == "pePolicy.php") echo 'class="selected"' ?>><a
         href="../../../src/pages/Indicators/pePolicy.php?id=<?php echo $_GET["id"]; ?>">Physical education
         policy</a></li>
-    <li <?php if (basename($_SERVER['PHP_SELF']) == "physicalEducationMonitoring.php") echo 'class="selected"' ?>><a
+    <li <?php if (basename($_SERVER['PHP_SELF']) == "peMonitoring.php") echo 'class="selected"' ?>><a
         href="../../../src/pages/Indicators/peMonitoring.php?id=<?php echo $_GET["id"]; ?>">Physical education
         monitoring</a></li>
-    <li <?php if (basename($_SERVER['PHP_SELF']) == "research.php") echo 'class="selected"' ?>><a
+    <li <?php if (basename($_SERVER['PHP_SELF']) == "researchPe.php") echo 'class="selected"' ?>><a
         href="../../../src/pages/Indicators/researchPe.php?id=<?php echo $_GET["id"]; ?>">Research in
         PE and school-based PA</a></li>
     <li <?php if (basename($_SERVER['PHP_SELF']) == "conclusion.php") echo 'class="selected"' ?>><a
