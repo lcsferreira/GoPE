182a1d5e0cbbdb07aaa475ddb1a480da1b8f6d44
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2024-05-14 09:44:23 -0300

    fix: add new text comment

--- src/components/commentGroup.php
+++ src/components/commentGroup.php
@@ -1,12 +1,13 @@
 <div class="indicator-comments">
-  <button class="btn-hide-comment" onclick="hideComment('<?php echo $indicatorOrder; ?>-comments')"><strong>Hide</strong>
+  <button class="btn-hide-comment"
+    onclick="hideComment('<?php echo $indicatorOrder; ?>-comments')"><strong>Hide</strong>
     Comments</button>
   <div id="<?php echo $indicatorOrder; ?>-comments" style="display: flex; flex-direction: column; gap: 1rem;">
-    <label for="<?php echo $indicatorName; ?>-comments">If any adjustment, please indicate year of information and
-      provide
-      additional
-      comments here:</label>
-    <textarea name="<?php echo $indicatorName; ?>-comments" id="<?php echo $indicatorName; ?>-comments" cols="20" rows="5" onblur="saveComment('<?php echo $indicatorName; ?>', '<?php echo $tableName; ?>')"><?php if ($commentValues[$indicatorName]) {
+    <label for="<?php echo $indicatorName; ?>-comments">If any adjustment is needed, provide additional comments
+      here: </label>
+    <textarea name="<?php echo $indicatorName; ?>-comments" id="<?php echo $indicatorName; ?>-comments" cols="20"
+      rows="5"
+      onblur="saveComment('<?php echo $indicatorName; ?>', '<?php echo $tableName; ?>')"><?php if ($commentValues[$indicatorName]) {
                                                                                                                                                                                                                 echo $commentValues[$indicatorName];
                                                                                                                                                                                                               } else {
                                                                                                                                                                                                                 echo "";
